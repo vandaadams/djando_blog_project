1bc07ea2399aa2e85024e5f2bccab0b730dfd08e
Vanda Adams <vandamadams@gmail.com>
2020-06-26 17:15:31 +0100

    CRUD class based views

---
+++ blogproject/blog/urls.py
@@ -0,0 +1,10 @@
+from django.urls import url
+from . import views
+
+urlpatterns = [
+    path('',views.PostListView.as_view(),name='post_list'),
+    path('about/',views.AboutView.as_view(),name='about'),
+    path('post/<int:pk>/',views.PostDetailView.as_view(),name='post_detail'),
+    path('post/new/',views.CreatePostView.as_view(),name='post_new'),
+    path('post/<int:pk>/edit/',views.PostUpdateView.as_view(),name='post_edit'),
+]
